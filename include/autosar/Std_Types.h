a5a68591c128ec81d565fc129432c457eb850ad4
ruang <longjinyii@outlook.com>
2024-05-24 14:41:06 +0800

    added AUTOSAR Driver interface support

---
+++ include/autosar/Std_Types.h
@@ -0,0 +1,6 @@
+#ifndef BENBEN_AUTOSAR_STD_TYPES_H
+#define BENBEN_AUTOSAR_STD_TYPES_H
+
+typedef char *Std_VersionInfoType;
+
+#endif // BENBEN_AUTOSAR_STD_TYPES_H
---
+++ include/autosar/cfg.h
@@ -0,0 +1,11 @@
+#ifndef BENBEN_AUTOSAR_CFG_H
+#define BENBEN_AUTOSAR_CFG_H
+
+// hardware port bit width
+// 8bit        0U
+// 16bit       1U
+// 32bit       2U
+#define PORT_PORTPIN_SIZE           1U
+#define PORT_PINMODE_SIZE           1U
+
+#endif // BENBEN_AUTOSAR_CFG_H
